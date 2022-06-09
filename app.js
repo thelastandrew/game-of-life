78c7e51c44fe10b10ff5e1eaf06a484d8a512cd1
thelastandrew <thelastandrew@gmail.com>
2022-06-09 06:50:10 +0300

    feat: implement start func withch runs the game

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const field = document.querySelector('.field');
 const ctx = field.getContext('2d');
+const submitBtn = document.querySelector('button');
 field.width = 500;
 field.height = 500;
 const cellRes = 10;
@@ -8,8 +9,6 @@ let cols = field.width / cellRes;
 let gen = [];
 fillRect();
 createLife();
-
-const submitBtn = document.querySelector('button');
 
 submitBtn.onclick = function () {
   const fieldWidth = document.getElementById('field-width').value;
@@ -62,4 +61,46 @@ function drawCell() {
       }
     }
   }
+}
+
+function start() {
+  let nextGen = [];
+  for (let i = 0; i < rows; i++) {
+    nextGen[i] = [];
+    for (let j = 0; j < cols; j++) {
+      let neighbors = 0;
+      if (gen[cu(i, rows) - 1][j] === 1) neighbors++; // check top
+      if (gen[co(i, rows) + 1][j] === 1) neighbors++; // check bottom
+      if (gen[i][cu(j, cols) - 1] === 1) neighbors++; // check left
+      if (gen[i][co(j, cols) + 1] === 1) neighbors++; // check right
+      if (gen[cu(i, rows) - 1][cu(j, cols) - 1] === 1) neighbors++; // check top left
+      if (gen[cu(i, rows) - 1][co(j, cols) + 1] === 1) neighbors++; //check top fight
+      if (gen[co(i, rows) + 1][cu(j, cols) - 1] === 1) neighbors++; // check bottom left
+      if (gen[co(i, rows) + 1][co(j, cols) + 1] === 1) neighbors++; // check bottom right
+
+      if (gen[i][j] === 0) {
+        neighbors === 3 ? (nextGen[i][j] = 1) : (nextGen[i][j] = 0);
+      } else {
+        neighbors === 2 || neighbors === 3
+          ? (nextGen[i][j] = 1)
+          : (nextGen[i][j] = 0);
+      }
+    }
+  }
+
+  gen = nextGen;
+  drawCell();
+  requestAnimationFrame(start);
+}
+
+// check under edge
+function cu(i, edge) {
+  if (i === 0) return edge;
+  else return i;
+}
+
+// check over edge
+function co(i, edge) {
+  if (i === edge - 1) return -1;
+  else return i;
 }
